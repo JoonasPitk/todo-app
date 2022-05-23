580347e38e82e2cf69ff8e6dfc36fa4603504d2a
JoonasPitk <46161@edu.raseko.fi>
2022-05-23 14:57:50 +0300

    Add tests.

---
+++ tehtavat/tests/test_models.py
@@ -0,0 +1,44 @@
+import pytest
+from tehtavat.models import Kategoria
+
+
+@pytest.mark.parametrize('nimi', ['', 'asd'])
+def test_kategoria_init(nimi):
+    kategoria = Kategoria(nimi=nimi)
+
+    assert kategoria.nimi == nimi
+
+@pytest.mark.django_db
+def test_kategoria_create():
+    kategoria = Kategoria.objects.create(nimi='moi')
+
+    assert kategoria.id, 'Pitäisi olla id, eli on tallennettu tietokantaan.'
+
+@pytest.mark.django_db
+def test_kategoria_tallennus():
+    kategoria = Kategoria(nimi='moi')
+
+    # Ennen tallennusta in on None.
+    assert kategoria.id is None
+
+    kategoria.save()
+
+    assert kategoria.nimi == 'moi'
+    assert kategoria.id
+    assert isinstance(kategoria.id, int)
+
+@pytest.mark.parametrize('filtteri,odotettu_tulos', [
+    (dict (nimi__contains='o'), {'moi'}),
+    (dict (nimi__contains='i'), {'moi', 'hei'}),
+    (dict (nimi__startswith='h'), {'hei'}),
+    (dict (nimi__startswith='h', nimi__contains='o'), set()),
+])
+
+@pytest.mark.django_db
+def test_kategoria_filter(filtteri, odotettu_tulos):
+    kategoria1 = Kategoria.objects.create(nimi='moi')
+    kategoria2 = Kategoria.objects.create(nimi='hei')
+
+    tulos = Kategoria.objects.filter(**filtteri)
+
+    assert set(x.nimi for x in tulos) == odotettu_tulos
---
+++ tehtavat/tests/test_views.py
@@ -0,0 +1,40 @@
+import pytest
+
+from django.http import HttpResponse
+from django.urls import reverse
+
+from tehtavat.models import Tehtava
+
+@pytest.mark.django_db
+def test_etusivu(client):
+    url = reverse('etusivu')
+
+    tulos = client.get(url)
+
+    assert isinstance(tulos, HttpResponse)
+    assert tulos.status_code == 200
+    assert 'Etusivu' in tulos.content.decode('utf-8')
+
+
+@pytest.mark.django_db
+def test_tehtavasivu(client):
+    tehtava = Tehtava.objects.create(otsikko='Aja testit')
+    url = reverse('tehtava', kwargs={'id': tehtava.id})
+
+    vastaus = client.get(url)
+
+    assert isinstance(vastaus, HttpResponse)
+    assert vastaus.status_code == 200
+    html_sisalto = vastaus.content.decode('utf-8')
+    assert '<title>TODO-ohjelma - Tehtävä - Aja testit</title>' in html_sisalto
+
+
+@pytest.mark.django_db
+def test_tehtava_sivua_ei_loydy(client):
+    url = reverse('tehtava', kwargs={'id': 0})
+
+    vastaus = client.get(url)
+
+    assert isinstance(vastaus, HttpResponse)
+    assert vastaus.status_code == 404
+    assert 'Tehtävää 0 ei löydy' in vastaus.content.decode('utf-8')
